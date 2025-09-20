75a3aed140d1213ea6f3d36c8b82f24ae646a2d1
Avishkar Kamble <aavishkark@gmail.com>
2025-09-20 23:43:30 +0530

    Added some animation to hero section

--- Constants/Index.js
+++ Constants/Index.js
@@ -1,23 +1,18 @@
 const Slides = [{
     id: '1',
-    heading:'The Papaya Takeover',
-    subHeading:'With Oscar and lando leading the Championship see how Maclaren is dominating the 2025 Season.',
+    heading:'The Papaya Domination',
+    subHeading:'With Oscar and lando leading the Championship, Check our analysis on how Mclaren is dominating the 2025 Season.',
     src:'/Images/oscar.png',
 },{
     id: '2',
     heading:'Max Sets Record',
     subHeading:'Max Verstappen sets the record for the fastest lap in Formula 1 history while qualifying for the 2025 Italian Grand Prix at Monza',
-    src:'/Images/oscar.png',
+    src:'/Images/max.png',
 },{
     id: '3',
-    heading:'The Papaya Takeover',
-    subHeading:'With Oscar and lando leading the Championship see how Maclaren is dominating the 2025 Season.',
-    src:'/Images/oscar.png',
-},{
-    id: '4',
-    heading:'Max Sets Record',
-    subHeading:'Max Verstappen sets the record for the fastest lap in Formula 1 history while qualifying for the 2025 Italian Grand Prix at Monza',
-    src:'/Images/oscar.png',
+    heading:"Lewis's Ferrari Experience",
+    subHeading:'Can Lewis Hamilton win his eighth world title at Ferrari? SF isn’t just another team, it’s where legends are made, but also where pressure is unparalleled.',
+    src:'/Images/lewis.png',
 }];
 
 export default Slides
--- src/Components/Slides/Slides.jsx
+++ src/Components/Slides/Slides.jsx
@@ -1,24 +1,52 @@
 import React, { useState } from 'react';
 import './slides.css';
 import { useGSAP } from '@gsap/react';
-import gsap from 'gsap/all';
+import gsap, { SplitText } from 'gsap/all';
+import _SplitText from 'gsap/SplitText';
 const Slide = (Slides) => {
-        let [num, setnum] = useState(1);
+    let [num, setnum] = useState(1);
 
-        const slides = Slides.Slides;
+    const slides = Slides.Slides;
 
-        console.log(slides)
 
-        let [activeslide, setactiveslide] = useState(slides[num-1]);
+    let [activeslide, setactiveslide] = useState(slides[num-1]);
 
-        useGSAP(()=>{
-        gsap.fromTo('#slideImg',{
-            x: 600, y: 0
+    useGSAP(() => {
+        document.querySelectorAll('.heading, .subHeading').forEach(el => {
+            el.innerHTML = el.textContent;
+        });
+
+        const headingSplit = new SplitText('.heading', { types: 'chars' });
+        const subHeadingSplit = new SplitText('.subHeading', { types: 'lines' });
+
+        gsap.from(headingSplit.chars, {
+            x: -100,
+            skewX: 45,
+            opacity: 0,
+            ease: 'power4.out',
+            duration: 1,
+            stagger: 0.03,
+        });
+
+        gsap.fromTo(subHeadingSplit.lines, {
+            y: -50,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.1,
+            delay: 0.3,
         },
         {
-            x: 0, y: 0
-        })
-    });
+            y: 0,
+            opacity: 1 
+        });
+
+        gsap.fromTo('#slideImg',
+            { x: -500, y: -500, rotation: 0, scale: 26, opacity: 0 },
+            { x: 0, y: 0, rotation: 0, opacity: 1, scale: 1, duration: 0.5, ease: 'power' }
+        );
+
+        },[activeslide]);
+
 
     const updateSlide = (btn) => {
 
@@ -42,7 +70,12 @@ const Slide = (Slides) => {
         }
         setnum(newnum)
         setactiveslide(slides[newnum - 1]);
+    }
 
+    const dotClick = (num) =>{
+        let newnum = Number(num);
+        setnum(newnum);
+        setactiveslide(slides[newnum - 1])
     }
 
 
@@ -61,8 +94,8 @@ const Slide = (Slides) => {
         <div className='dots'>
             {slides.map((slide)=>
                 slide.id === activeslide.id ? 
-                <div className='dot active' key={slide.id}>{'o'}</div> :
-                <div className='dot' key={slide.id}>{'o'}</div>
+                <div className='dot active' key={slide.id} onClick={()=>{dotClick(slide.id)}}>{'o'}</div> :
+                <div className='dot' key={slide.id} onClick={()=>{dotClick(slide.id)}}>{'o'}</div>
             )}
         </div>
         <div className='arrows'>
